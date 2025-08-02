cc3e34bcf4ce3eacd28126d0185381b4ddc07ac9
dodonggu <167772411+dodonggu@users.noreply.github.com>
2025-08-02 21:07:00 +0900

    [Bronze V] Title: 사칙연산, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -0,0 +1,14 @@
+# 합, 차, 곱, 몫, 나눔
+
+a, b = map(int, input().split())
+
+sum = a + b
+diff = a - b
+product = a * b
+div = a // b
+mod = a % b
+print(sum)
+print(diff)
+print(product)
+print(div)
+print(mod)
