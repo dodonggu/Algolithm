f4b7b512c238052d5e9845d289563fc3bfb7cf74
dodonggu <167772411+dodonggu@users.noreply.github.com>
2025-08-03 17:38:11 +0900

    [Silver IV] Title: 배수 스위치, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/12927. 배수 스위치/배수 스위치.py
+++ Python/백준/Silver/12927. 배수 스위치/배수 스위치.py
@@ -1,35 +1,22 @@
-# 전구 리스트 입력 받음
-bulb_lst = list(input().upper()[:])
-# print(bulb_lst)
+# 전구 상태 입력받음
+bulb_state_list = list(input()[:])
+length = len(bulb_state_list)
 
-# 1과 0으로 상태 값 변환(나중에 상태 전환을 위해서)
-''' 잘못된 코드 예시
-for bulb in bulb_lst:
-    if bulb == 'Y':
-        bulb = 1
-    elif bulb == 'N':
-        bulb = 0
-'''        
-for i in range(len(bulb_lst)):
-    if bulb_lst[i] == 'Y':
-        bulb_lst[i] = 1
-    elif bulb_lst[i] == 'N':
-        bulb_lst[i] = 0
+# 'Y'와 'N'으로 이루어진 요소들 '1'과 '0'으로 변환
+# 나중에 상태 전환을 위해서
+for i in range(length):
+    if bulb_state_list[i] == 'Y':
+        bulb_state_list[i] = 1
+    elif bulb_state_list[i] == 'N':
+        bulb_state_list[i] = 0
 
-# print(bulb_lst)
-
-length = len(bulb_lst)
 count = 0
 
-# 모든 스위치 순회
-for i in range(1, length + 1):
-    # i번째 스위치가 켜져있다면
-    # index는 i-1
-    if bulb_lst[i - 1] == 1:
-        # i의 배수 번쨰 스위치 모두 상태 반전
-        for idx in range(i-1, length, i):
-            bulb_lst[idx] = 1 - bulb_lst[idx]
-        # print(bulb_lst)
+# 모든 전구 순회
+for i in range(length):
+    if bulb_state_list[i] == 1:
+        # (i+1)의 배수 번쨰 값 상태 전환
+        for idx in range(i, length, i+1):
+            bulb_state_list[idx] = 1 - bulb_state_list[idx]
         count += 1
-
 print(count)
