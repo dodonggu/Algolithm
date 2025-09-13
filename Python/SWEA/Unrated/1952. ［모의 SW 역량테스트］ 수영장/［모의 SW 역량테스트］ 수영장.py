3f58382737a9d2e24d0f5d4f3e1f79befd090040
dodonggu <167772411+dodonggu@users.noreply.github.com>
2025-09-13 18:40:10 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 63 ms, Memory: 51,328 KB -BaekjoonHub

--- Python/SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.py
+++ Python/SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.py
@@ -1,29 +1,23 @@
-def dfs(month, total):
-    global min_result
-
-    # 가지치기
-    if total >= min_result:
-        return
-
-    # 종료조건
-    if month > 12:
-        # 최소값 갱신
-        min_result = min(total, min_result)
-        return
-
-    # 4가지 이용권의 경우
-    dfs(month + 1, total + days[month] * cost_day)  # 하루
-    dfs(month + 1, total + cost_month)  # 한달
-    dfs(month + 3, total + cost_3month)  # 3개월
-    dfs(month + 12, total + cost_year)  # 1년
-
-
 T = int(input())
 for tc in range(1, T + 1):
-    # 각 이용권 가격
-    cost_day, cost_month, cost_3month, cost_year = map(int, input().split())
-    # 월별로 이용할 일 수
-    days = [0] + list(map(int, input().split()))  # 1월부터 시작
-    min_result = float("inf")
-    dfs(1, 0)  # 1월부터 시작
-    print(f"#{tc} {min_result}")
+    # 이용권 가격들
+    cost_day, cost_month, cost_month3, cost_year = map(int, input().split())
+    # 월별 이용 계획 일수
+    days = [0] + list(map(int, input().split()))
+
+    # 합을 기록해나갈 dp 리스트
+    dp = [0] * 13
+
+    dp[1] = min(days[1] * cost_day, cost_month)
+    dp[2] = dp[1] + min(days[2] * cost_day, cost_month)
+
+    for month in range(3, 13):
+        dp[month] = min(
+            dp[month - 1] + days[month] * cost_day,
+            dp[month - 1] + cost_month,
+            dp[month - 3] + cost_month3,
+        )
+
+    answer = min(dp[12], cost_year)
+
+    print(f"#{tc} {answer}")
