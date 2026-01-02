8a51ad21232218da26b4fd413d3b02e254ada109
dodonggu <167772411+dodonggu@users.noreply.github.com>
2026-01-03 00:30:39 +0900

    [Gold V] Title: 용액, Time: 112 ms, Memory: 44748 KB -BaekjoonHub

--- Python/백준/Gold/2467. 용액/용액.py
+++ Python/백준/Gold/2467. 용액/용액.py
@@ -1,36 +1,30 @@
 import sys
-
 input = sys.stdin.readline
 # sys.stdin = open('input.txt', 'r')
 
 N = int(input())
 arr = list(map(int, input().split()))
+# arr.sort() 오름차순 보장
 
 # 투 포인터를 위한 변수 초기화
-best_i = 0
-best_j = N - 1
-best_sum = arr[0] + arr[N - 1]
+i, j = 0, N - 1
+best_i, best_j = i, j
+best_val = arr[i] + arr[j]
 
-# 포인터 이동
-i = 0
-j = N - 1
-for _ in range(N - 1):  # 배열 모두 순회
-    new_sum = arr[i] + arr[j]
+# 교차 전까지 반복
+while i < j:
+    cur_val = arr[i] + arr[j]
 
     # 새로운 값이 기존 값보다 더 좋을 경우
-    if abs(new_sum) < abs(best_sum):
-        best_sum = new_sum
-        best_i = i
-        best_j = j
-    # print(f'i = {arr[i]}, j = {arr[j]}, sum = {new_sum}')
+    if abs(cur_val) < abs(best_val):
+        best_val = cur_val
+        best_i, best_j = i, j
 
-    if new_sum == 0:
-        best_i = i
-        best_j = j
+    if cur_val == 0:
         break
-    elif new_sum < 0:
+    elif cur_val < 0:
         i += 1
     else:
         j -= 1
 
-print(f'{arr[best_i]} {arr[best_j]}')
+print(arr[best_i], arr[best_j])
