18a23925d9f95a2a0ee1e0c7860ff8619789a259
dodonggu <167772411+dodonggu@users.noreply.github.com>
2026-01-04 00:18:43 +0900

    [Silver III] Title: 퇴사, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/14501. 퇴사/퇴사.py
+++ Python/백준/Silver/14501. 퇴사/퇴사.py
@@ -1,27 +1,41 @@
 import sys
 
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 # sys.stdin = open('input.txt', 'r')
 
-
-def dfs(day, profit):
-
-    if day == N + 1:
-        global best_profit
-        best_profit = max(profit, best_profit)
-        return
-
-    # 그 날에 일을 하지 않았을 경우
-    dfs(day + 1, profit)
-
-    # 그 날에 일을 했을 경우
-    if day + arr[day][0] <= N + 1:
-        dfs(day + arr[day][0], profit + arr[day][1])
-
-
+# 1. 입력 및 데이터 구조화
 N = int(input())
-arr = [[0]] + [list(map(int, input().split())) for _ in range(N)]  # 1-based paddng
+# 1-based index [0] padding
+# 구조 분해 할당
+T = [0] * (N + 1)
+P = [0] * (N + 1)
+for i in range(1, N + 1):
+    T[i], P[i] = map(int, input().split())
 
-best_profit = 0
-dfs(1, 0)  # (날짜, 수익) 초기값은 각각 1일과 0원
-print(best_profit)
+# 해당 날짜에서 얻을 수 있는 최대 수익 저장
+# N+1일 경계값까지 넉넉하게 패딩
+memo = [-1] * (N + 2)
+
+def dfs(day):
+    # 2. 기저 조건 (N+1일은 퇴사일이므로 수익 0)
+    if day > N:
+        return 0
+
+    # 3. memoization
+    if memo[day] != -1:
+        return memo[day]
+
+    # 4. 점화식 logic
+
+    # Case A: 오늘 일을 안 함
+    res = dfs(day + 1)
+
+    # Case B: 오늘 일을 함 (경계 조건 체크)
+    if day + T[day] <= N + 1:
+        res = max(res, P[day] + dfs(day + T[day]))
+
+    memo[day] = res
+    return res
+
+print(dfs(1))
