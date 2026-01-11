dac78eae35072c9b750a17bf0f1b4ba796f3dcf0
dodonggu <167772411+dodonggu@users.noreply.github.com>
2026-01-11 22:08:12 +0900

    [Gold IV] Title: 플로이드, Time: 532 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -0,0 +1,42 @@
+import sys
+
+# 입력 속도를 높이기 위해 sys.stdin.readline 사용
+input = sys.stdin.readline
+INF = int(1e9) # 무한을 의미하는 값으로 10억 설정
+
+# 노드의 개수(n)와 간선의 개수(m) 입력
+n = int(input())
+m = int(input())
+
+# 2차원 리스트(그래프)를 만들고, 모든 값을 무한으로 초기화
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+# 자기 자신에서 자신으로 가는 비용은 0으로 초기화
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if a == b:
+            graph[a][b] = 0
+
+# 각 간선에 대한 정보를 입력받아, 그 값으로 초기화
+for _ in range(m):
+    # A에서 B로 가는 비용 C
+    a, b, c = map(int, input().split())
+    # 가장 짧은 간선 정보만 저장 (중복 간선 처리)
+    if c < graph[a][b]:
+        graph[a][b] = c
+
+# 점화식에 따라 플로이드 워셜 알고리즘 수행
+for k in range(1, n + 1): # 거쳐가는 노드
+    for a in range(1, n + 1): # 출발 노드
+        for b in range(1, n + 1): # 도착 노드
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+# 수행된 결과를 출력
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        # 도달할 수 없는 경우 0을 출력
+        if graph[a][b] == INF:
+            print(0, end=" ")
+        else:
+            print(graph[a][b], end=" ")
+    print()
