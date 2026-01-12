d3583820eecb26105b26ea1a9e635fed2f1ef1f7
dodonggu <167772411+dodonggu@users.noreply.github.com>
2026-01-13 00:19:06 +0900

    [Gold V] Title: 적록색약, Time: 68 ms, Memory: 35140 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,65 +1,66 @@
 import sys
-from collections import deque
+from collections import  deque
 
-# 로컬 테스트용 (제출 시에는 주석 처리하거나 삭제하세요)
 # sys.stdin = open('input2.txt', 'r')
 input = sys.stdin.readline
 
-# 상하좌우 이동을 위한 벡터
 dy = [-1, 1, 0, 0]
 dx = [0, 0, -1, 1]
 
 def bfs(y, x, visited, is_rg):
     q = deque([(y, x)])
     visited[y][x] = True
-    current_color = grid[y][x]
-    
+    cur_color = grid[y][x]
+
     while q:
         cy, cx = q.popleft()
-        
-        for i in range(4):
-            ny, nx = cy + dy[i], cx + dx[i]
-            
-            if 0 <= ny < N and 0 <= nx < N and not visited[ny][nx]:
-                target_color = grid[ny][nx]
-                
-                if not is_rg:
-                    # 일반인: 색상이 정확히 일치해야 함
-                    if target_color == current_color:
+
+        for d in range(4):
+            ny, nx = cy + dy[d], cx + dx[d]
+
+            if not (0 <= ny < N and 0 <= nx < N):
+                continue
+
+            if visited[ny][nx]:
+                continue
+
+            nxt_color = grid[ny][nx]
+
+            if is_rg:
+                # 적록색약인 경우: R과 G를 한 묶음으로 취급
+                if cur_color in ['R', 'G']:
+                    if nxt_color in ['R', 'G']:
+                        q.append((ny, nx))  # 3. 튜플로 append
                         visited[ny][nx] = True
-                        q.append((ny, nx))
                 else:
-                    # 적록색약: R과 G를 같은 것으로 취급
-                    if current_color == 'R' or current_color == 'G':
-                        if target_color == 'R' or target_color == 'G':
-                            visited[ny][nx] = True
-                            q.append((ny, nx))
-                    else:
-                        # 파란색은 일반인과 동일하게 처리
-                        if target_color == current_color:
-                            visited[ny][nx] = True
-                            q.append((ny, nx))
+                    # 파란색인 경우
+                    if cur_color == nxt_color:
+                        q.append((ny, nx))
+                        visited[ny][nx] = True
+            else:
+                # 일반인인 경우: 색이 정확히 같아야 함
+                if cur_color == nxt_color:
+                    q.append((ny, nx))
+                    visited[ny][nx] = True
+
 
 N = int(input())
 grid = [list(input().strip()) for _ in range(N)]
-
-# 방문 여부를 체크할 배열을 각각 생성
 visited_normal = [[False] * N for _ in range(N)]
 visited_rg = [[False] * N for _ in range(N)]
 
-normal_count = 0
-rg_count = 0
-
+normal_cnt = 0
+rg_cnt = 0
 for y in range(N):
     for x in range(N):
-        # 일반인 기준 구역 탐색
+        # 방문하지 않은 노드를 탐색
         if not visited_normal[y][x]:
             bfs(y, x, visited_normal, False)
-            normal_count += 1
-            
-        # 적록색약 기준 구역 탐색
+            normal_cnt += 1
+
         if not visited_rg[y][x]:
             bfs(y, x, visited_rg, True)
-            rg_count += 1
+            rg_cnt += 1
 
-print(f'{normal_count} {rg_count}')
+
+print(f'{normal_cnt} {rg_cnt}')
